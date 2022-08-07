b15c10e880a96e4c47a6b21c1b814623ce2c6ba4
babyt <jiyeecries@gmail.com>
2022-08-08 01:14:05 +0800

    landing page static top

--- src/layouts/landing/Landing.tsx
+++ src/layouts/landing/Landing.tsx
@@ -1,16 +1,60 @@
 import { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Button } from '@chakra-ui/react';
+import { 
+        Button,
+        Flex,
+        Heading,
+        Image,
+        Stack,
+        Text,
+        useBreakpointValue, 
+        } from '@chakra-ui/react';
 
 const Landing: FC = () => {
     const navigate = useNavigate();
     return (
         <div>
-            <h1>Landing</h1>
-            <Button mt={2} onClick={() => navigate("/home")}>
-                go to home
-            </Button>
+            <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+            <Flex p={6} flex={1} align={'center'} m={[2,9]}>
+                <Stack spacing={6} w={'full'} maxW={'lg'}>
+                <Heading fontSize={{ base: '3xl', sm: '4xl', md: '9xl' }}>
+                    <Text
+                    as={'span'}
+                    position={'relative'}
+                    _after={{
+                        content: "''",
+                        width: 'full',
+                        height: useBreakpointValue({ base: '20%', md: '30%' }),
+                        position: 'absolute',
+                        bottom: 1,
+                        left: 0,
+                        bg: 'blue.400',
+                        zIndex: -1,
+                    }}>
+                    splash
+                    </Text>
+                    <br />{' '}
+                </Heading>
+                <Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+                        Live streaming video platform
+                </Text>
+                <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+                    <Button rounded={'full'} onClick={() => navigate("/home")}>Go to Home</Button>
+                </Stack>
+                </Stack>
+            </Flex>
+            {/* <Flex flex={1}>
+                <Image
+                alt={'Login Image'}
+                objectFit={'cover'}
+                src={
+                    'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                }
+                />
+            </Flex> */}
+            </Stack>
+
         </div>
     );
 };
