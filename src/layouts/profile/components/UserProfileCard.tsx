8df5681a465a6e932804f7f18c643d973b14a366
shanta <teirenxuanshanta@gmail.com>
2022-08-11 20:18:19 +0800

    made follow button responsive

--- src/layouts/profile/components/UserProfileCard.tsx
+++ src/layouts/profile/components/UserProfileCard.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import { Box, Avatar, Center, VStack, Heading, Text, Button } from '@chakra-ui/react'
+import { Box, Avatar, Center, VStack, Heading, Text, Button,useMediaQuery } from '@chakra-ui/react'
 
 const UserProfileCard = () => {
+
+    const [isLargerThan830] = useMediaQuery('(min-width: 830px)')
+
     return (
         <Box
             w={"100%"}
@@ -9,15 +12,9 @@ const UserProfileCard = () => {
             height={80}
             position={'relative'}
             mt={2}
-            
+
         >
-            <Button position={'absolute'}
-                top={5}
-                right={5}
-                colorScheme='teal'
-                >
-                Follow
-            </Button>
+
             <Center>
                 <Avatar
                     size='2xl'
@@ -27,10 +24,10 @@ const UserProfileCard = () => {
                 />
             </Center>
             <VStack align={"center"}>
-                <Heading as='h2' size='2xl' noOfLines={1} maxW='50%'>
+                <Heading size='2xl' noOfLines={1} maxW='50%'>
                     Dan Abramov
                 </Heading>
-                <Text fontSize='md' noOfLines={3} maxW='50%' fontWeight={'light'}>
+                <Text noOfLines={3} maxW='50%' fontWeight={'light'}>
                     Streamer Bio Here
                 </Text>
 
@@ -38,6 +35,19 @@ const UserProfileCard = () => {
                     2.6k Followers
                 </Text>
 
+                <Button
+                    // sx={{
+                    //     'font-size': 'clamp(0.7rem,calc(0.7rem+5vw), 2rem)!important',
+                    //     padding: 'calc(clamp(0.5rem,calc(0.7rem+2vw),1rem) / 2) clamp(0.5rem,calc(0.7rem+2vw),1rem) !important'
+                    // }}
+                    position={isLargerThan830? 'absolute': 'relative'}
+                    top={isLargerThan830?3:0}
+                    right={isLargerThan830?5:0}
+                    minWidth={'1rem'}
+                    colorScheme='teal'
+                >
+                    Follow
+                </Button>
             </VStack>
         </Box>
     )
