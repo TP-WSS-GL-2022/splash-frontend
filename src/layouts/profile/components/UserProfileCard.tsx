5cdcd9964f9056ca2b04808f5f2a0dbf27500d69
shanta <teirenxuanshanta@gmail.com>
2022-08-11 20:51:35 +0800

    made card look nicer

--- src/layouts/profile/components/UserProfileCard.tsx
+++ src/layouts/profile/components/UserProfileCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Avatar, Center, VStack, Heading, Text, Button,useMediaQuery } from '@chakra-ui/react'
+import { Box, Avatar, Center, VStack, Heading, Text, Button, useMediaQuery } from '@chakra-ui/react'
 
 const UserProfileCard = () => {
 
@@ -9,10 +9,10 @@ const UserProfileCard = () => {
         <Box
             w={"100%"}
             bgColor="blackAlpha.300"
-            height={80}
+            minHeight={80}
             position={'relative'}
             mt={2}
-
+            marginBottom={5}
         >
 
             <Center>
@@ -23,26 +23,20 @@ const UserProfileCard = () => {
                     src="https://bit.ly/dan-abramov"
                 />
             </Center>
-            <VStack align={"center"}>
-                <Heading size='2xl' noOfLines={1} maxW='50%'>
+            <VStack align={"center"} paddingBottom={3}>
+                <Heading size={isLargerThan830 ? '2xl' : 'xl'}  >
                     Dan Abramov
                 </Heading>
-                <Text noOfLines={3} maxW='50%' fontWeight={'light'}>
-                    Streamer Bio Here
-                </Text>
-
                 <Text>
                     2.6k Followers
                 </Text>
-
+                <Text maxW={'80%'} >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, eius ex? Quos in nam neque dolor a eaque, quam aut dicta quasi nobis aliquid corporis laborum rem explicabo eveniet! Voluptate.
+                </Text>
                 <Button
-                    // sx={{
-                    //     'font-size': 'clamp(0.7rem,calc(0.7rem+5vw), 2rem)!important',
-                    //     padding: 'calc(clamp(0.5rem,calc(0.7rem+2vw),1rem) / 2) clamp(0.5rem,calc(0.7rem+2vw),1rem) !important'
-                    // }}
-                    position={isLargerThan830? 'absolute': 'relative'}
-                    top={isLargerThan830?3:0}
-                    right={isLargerThan830?5:0}
+                    position={isLargerThan830 ? 'absolute' : 'relative'}
+                    top={isLargerThan830 ? 3 : 0}
+                    right={isLargerThan830 ? 5 : 0}
                     minWidth={'1rem'}
                     colorScheme='teal'
                 >
