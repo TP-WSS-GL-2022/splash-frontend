17510ca269061d998bded2b528bfd554aec6a190
Zachary Lim <zacharylim2004@gmail.com>
2022-08-12 07:39:35 +0800

    ✨ finished recommended smol channels

--- src/components/ProfileThumbnail.tsx
+++ src/components/ProfileThumbnail.tsx
@@ -1,17 +1,52 @@
-import { Box, useColorModeValue } from "@chakra-ui/react";
+import {
+    Avatar,
+    Box,
+    Image,
+    Container,
+    Text,
+    useColorModeValue,
+    VStack,
+} from "@chakra-ui/react";
 import { FC } from "react";
 import { User } from "../models";
 
-const ProfileThumbnail: FC = () => {
+const ProfileThumbnail: FC<{ user: User }> = ({ user }) => {
     return (
-        <Box
+        <Container
+            my={2}
             p={8}
             rounded={"lg"}
             boxShadow={"lg"}
-            bg={useColorModeValue("white", "gray.700")}
+            bg={useColorModeValue("white", "gray.800")}
+            transition="all 0.2s"
+            borderWidth="thin"
+            borderColor="gray.700"
+            _hover={{
+                transform: "scale(1.02)",
+                borderColor: "gray.600",
+                bgColor: "gray.700",
+            }}
         >
-            asdasd
-        </Box>
+            <VStack>
+                <Avatar
+                    border={useColorModeValue(
+                        "4px solid white",
+                        "4px solid var(--chakra-colors-gray-700)"
+                    )}
+                    size={"xl"}
+                >
+                    {user.photo && (
+                        <Image
+                            borderRadius="full"
+                            boxSize="full"
+                            src={user.photo}
+                        />
+                    )}
+                </Avatar>
+                <Text>{user.username}</Text>
+                <Text>{user.followerCount} followers</Text>
+            </VStack>
+        </Container>
     );
 };
 
